e11ee728571af7d1104b23f2bd3a9efaa62cdaa2
sreejitha-mc <sreejitha.mc19@gmail.com>
2020-07-31 12:06:07 +0530

    initial update cert-issuer

--- start-cert-issuer.sh
+++ start-cert-issuer.sh
@@ -4,22 +4,22 @@
 set -e
 
 # start the bitcoin service
-bitcoind -daemon
+bitcoind -regtest -daemon
 
 # wait for bitcoind to start accepting connections
 while ! nc -z localhost 8332 </dev/null; do sleep 10; done
 
 # Create an issuing address and save the output
-ISSUER=$(bitcoin-cli getnewaddress "" legacy)
+ISSUER=$(bitcoin-cli -regtest getnewaddress "" legacy)
 sed -i.bak "s/<issuing-address>/$ISSUER/g" /etc/cert-issuer/conf.ini
-KEY="$(bitcoin-cli dumpprivkey $ISSUER)"
+KEY="$(bitcoin-cli -regtest dumpprivkey $ISSUER)"
 echo $KEY > /etc/cert-issuer/pk_issuer.txt
 
 # advance network
-bitcoin-cli generate 101
+bitcoin-cli -regtest generatetoaddress 101 $ISSUER
 
 # send btc to issuer address
-bitcoin-cli sendtoaddress $ISSUER 5
+bitcoin-cli -regtest sendtoaddress $ISSUER 5
 
 # start web service
 service nginx start && uwsgi --ini wsgi.ini
